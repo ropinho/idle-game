2dec9c3d0a99bdab9bf42776207de1cf3d353aa4
Ronaldd Pinho <ronaldppinho@gmail.com>
2018-09-18 12:09:29 -0300

    Atualizando diagramas e adicionando pacote idle/item/

--- src/Mochila.java
+++ src/Mochila.java
@@ -1,12 +1,15 @@
-
+import idle.item.Item;
 /*
  * A idéia é que a Mochila possa ser expandida de acordo com
  * a mudança de nivel (evolução), daí */
 public class Mochila {
 	protected int level;
-    protected Item[] itens;
+	protected int size;
+  protected Item[] itens;
 
-    public Mochila(){
-    	/* constructor */
-    }
+  public Mochila(int l){
+  	this.level = l;
+		this.size = 16;
+		this.itens = new Item[this.size];
+  }
 }
--- src/idle/item/Item.java
+++ src/idle/item/Item.java
@@ -1,7 +1,8 @@
+package idle.item;
+
 public class Item {
 	/* classe item tem os atributos comuns aos Itens como:
 	Valor de incremento em atributos do personagem, peso ... */
-
 	protected float peso;
-	protected int incrForca;
+	
 }
