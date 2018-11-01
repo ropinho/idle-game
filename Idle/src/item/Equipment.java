6a68ff006f88224d8c0a1d9603daca2f1481b49e
wagnerfns <wagnerfelidre@gmail.com>
2018-10-31 22:53:36 -0300

    Implement loader equipment

--- Idle/src/item/Equipment.java
+++ Idle/src/item/Equipment.java
@@ -1,17 +1,47 @@
 package item;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import personagem.Hero;
+
 public class Equipment {
-    
-    
-    /*
-    verificar se no array list póde contar os idices
-    
-    fazer elementos:
-    
-    Elmo
-    luva
-    armadura
-    bota
-    arma : espada | cajado | arco
-    */
+	private int[] Attribute;
+
+	public void equiparPersonagem(Hero person){
+		person.showAttributes();
+		
+	}	
+
+	public void loaderEquipment(){
+		String[][] equipment = new String[3][3];
+		//String line;
+		
+		
+		try {
+			FileReader file = new FileReader("src/item/dados.csv");
+			
+			BufferedReader readFile = new BufferedReader(file);
+			
+			int i = 0;
+			equipment[i] = readFile.readLine().split(",");
+			
+			while(equipment != null){
+				
+				for(String line : equipment[i]) {
+					System.out.println(line);
+				}
+		
+				
+				System.out.println();
+				i++;
+				equipment[i] = readFile.readLine().split(",");
+			}
+			file.close();
+		} catch(IOException e) {
+			System.out.println("Error bixo");
+		}
+		
+	}
 }
