0c8086c5e6142c70e9078b39a6b7d8368390baa2
wagnerfns <wagnerfelidre@gmail.com>
2018-11-29 15:33:14 -0300

    geter and setter

--- src/item/Item.java
+++ src/item/Item.java
@@ -5,10 +5,33 @@ public class Item {
     int level;
     String classChar;
 
-
     public Item(String name, int lvl, String classChar) {
         this.name = name;
         this.level = lvl;
         this.classChar = classChar;
-}
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLevel() {
+        return this.level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getClassChar() {
+        return this.classChar;
+    }
+
+    public void setClassChar(String classChar) {
+        this.classChar = classChar;
+    }
 }
