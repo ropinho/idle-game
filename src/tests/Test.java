1f95cd2095aaa84b6bc5021e74edb56d486714fc
Ronaldd Pinho <ronaldppinho@gmail.com>
2018-12-03 20:04:49 -0300

    classe de Teste: apaguei o main() pra evitar conflitos

--- src/tests/Test.java
+++ src/tests/Test.java
@@ -11,57 +11,8 @@ public class Test {
 	
   private static Scanner in;
   private static Explore exploration;
-<<<<<<< HEAD
-
-  public static void main(String args[]){
-	in = new Scanner(System.in);
-
-    // criando o personagem (Heŕoi)
-	/*
-    System.out.printf("\nNome do personagem: ");
-    String myname = in.nextLine();
-    System.out.printf("\nClasse do Herói: \n");
-    System.out.printf("[a] Arrow\n[s] Swordsman\n[m] Magician\nDigite: ");
-    String classechar = in.next();
-    */
-    Hero meuHeroi = Creator.createHero("s", "Ronaldd");
-
-    // distribuição dos pontos de atributos iniciais = 50
-    //Creator.distributePoints(50, meuHeroi);
-    
-    exploration = new Explore(meuHeroi);
-
-    System.out.println("\nSeu personagem:");
-    System.out.printf("Nome: %s\n", meuHeroi.getName());
-    System.out.printf("Classe: %s\n", meuHeroi.getJOB() );
-    meuHeroi.showAttributes();
-    System.out.printf("POWER: %d\n", meuHeroi.getPower());
-    System.out.printf("HP: %d\n\n", meuHeroi.getHp());
-
-    
-    System.out.println("Iniciar jogo? [y/n]");
-    String resp = in.next();
-    char r = resp.charAt(0);
-    
-    if (r != 'y') {
-    	System.out.println("Encerrando o jogo");
-    } else {
-    	exploration.start();
-    }
-=======
-  private static Hero meuHeroi;
   /*
   public static void main(String args[]){
-	in = new Scanner(System.in);
-
-    meuHeroi = Creator.createHero("s", "Aluisio Lee");
-    exploration = new Explore(meuHeroi);
-
-    Console.printAllCharacterInfo(meuHeroi);
-
-    /* Start the loop *
-    exploration.start();
->>>>>>> ronald
     
   }*/
 }
