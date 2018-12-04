47ac1436fc3aa2155168e499a99b052d8652b524
wagnerfns <wagnerfelidre@gmail.com>
2018-12-03 23:56:46 -0300

    Testando itens e mapa

--- src/tests/Test.java
+++ src/tests/Test.java
@@ -1,7 +1,23 @@
 package tests;
 
+
 import java.util.Scanner;
 
+import util.Creator;
+import util.Console;
+import item.Item;
+import map.Map;
+
 public class Test {
-  
+  private static Map mapa;
+  private static Item itens;
+  public static void main(String args[]){
+	  
+	  mapa = Creator.creatorMap(2);
+	  System.out.println("Fase: " + mapa.getFase());
+	  
+	  itens = Creator.generateEquipment(mapa.getLevel(), "s");
+	  
+	  Console.printEquip(itens);
+  }
 }
